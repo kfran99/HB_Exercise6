8602c168444c0236a932155a4e93da621ba83bc5
Hackbright Student <info@hackbrightacademy.com>
2014-02-10 18:02:30 -0800

    Words converted to lower-case and started to remove other characters not at end of words.

--- wordcount.py
+++ wordcount.py
@@ -1,28 +1,24 @@
-#open a file named on command line 
+#open and read file and then convert it all to lower case
 
-#print those counts to screen
-
-#open file
-#for line in twain: do something
 twain = open('twain.txt')
 twain_read = twain.read()
-    
+twain_read_lower = twain_read.lower()
+twain_replace = twain_read_lower.replace("--", " ")   
+
+for word in twain_read_lower:
+    if ord(word) > 122 or < 97:
+        word.replace()
+
 
 #count how many times each space-separated word occurs in file
 value = 0
 twain_dictionary = {}
 #split all space-separated words
-
    
-   
-for word in twain_read.split():
-     #clean up double dash connected words
-    #split words at double dashes
-    #cleaned_word = twain_read.split("-")
-
+for word in twain_replace.split():
     #stripping the punctuation from the document, but only if it is at the end of a word
     stripped_word = word.strip("!,.';:-\"?_")
-        #print stripped_word
+    #print stripped_word
     #modifies dictionary and adds the count
     twain_dictionary[stripped_word] = twain_dictionary.get(stripped_word, 0) + 1
  
